bf6c096a553afdf3b378e305f68be760fadec40e
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-06 13:19:07 -0300

    Cria testes para garantir rubysmo: truthy/falsey.

--- assert.rb
+++ assert.rb
@@ -33,12 +33,19 @@ end
 
 # daqui em diante tudo é teste :)
 
+# Garante que um bloco lança um exceção específica
 class NadaConstaError < StandardError; end
 assert_raise(NadaConstaError) { raise NadaConstaError.new }
 
+# Garante ser possível verificar que um bloco NÃO lança exceção
 assert_raise(AssertionError) do
   assert_not_raise { raise NadaConstaError.new }
 end
 
+# Garante que assert lança exceções para expecativas de valor `falsey`
 assert_raise(AssertionError) { assert false }
+assert_raise(AssertionError) { assert nil }
+
+# Garante que assert NÃO lança exceções se expecativas são `truthy`
 assert_not_raise { assert true }
+assert_not_raise { assert "yes" }
