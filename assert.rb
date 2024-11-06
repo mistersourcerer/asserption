3c0c319127a515f277a88bccbf7fe29f081d2d9f
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-06 13:28:52 -0300

    Cria teste de falha para exceção não esperada

--- assert.rb
+++ assert.rb
@@ -4,6 +4,12 @@ class AssertionNotRaising < StandardError
   end
 end
 
+class AssertionRaisingError < StandardError
+  def initialize(expected, raised)
+    super "Expected #{expected.name} to be raised, but was #{raised.class} instead"
+  end
+end
+
 class AssertionError < StandardError; end
 
 def assert_raise(error_type, &expectation)
@@ -16,6 +22,8 @@ def assert_raise(error_type, &expectation)
   rescue error_type
     # Nada a fazer, significa que o teste passou porque a execução da
     # expectation lançou a exceção esperada.
+  rescue StandardError => e
+    raise AssertionRaisingError.new error_type, e
   end
 end
 
@@ -40,6 +48,11 @@ assert_raise(NadaConstaError) { raise NadaConstaError.new }
 # Garante ser possível verificar que um bloco NÃO lança exceção
 assert_raise(AssertionError) do
   assert_not_raise { raise NadaConstaError.new }
+end
+
+class VasoRuimError < StandardError; end
+assert_raise(AssertionRaisingError) do
+  assert_raise(NadaConstaError) { raise VasoRuimError }
 end
 
 # Garante que assert lança exceções para expecativas de valor `falsey`
