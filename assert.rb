c45ea32ce767ef80a50f8f1b3a110d57161fdf62
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-06 01:26:35 -0300

    Faz passar o teste com expectation negativa
    A criação da exception AssertionError nos possibilita garantir que, se
    uma expectation for negativa (`falsey`), nosso framework de testes irá
    “falhar” o teste através dessa exceção.
    
    O que significa que esse é o primeiro teste oficialmente passando! E
    esse é um assert que funciona bem o suficiente para a maioria dos casos.
    
    Mas como gosto do esporte, quero forçar um pouco a barra e garantir que,
    se o valor da asserção não for `true`, ela deve falhar. Portanto não
    posso aceitar outros objetos como expectation positivas…

--- assert.rb
+++ assert.rb
@@ -1,7 +1,8 @@
 class AssertionNotRaising < StandardError; end
+class AssertionError < StandardError; end
 
 def assert(expectation)
-
+  raise AssertionError.new if !expectation
 end
 
 
@@ -13,4 +14,7 @@ begin
   assert false
 
   raise AssertionNotRaising.new
+rescue AssertionError
+  # Nada a fazer, `AssertionError` é o que queremos ver aqui! :)
+  # significa que o teste passou.
 end
