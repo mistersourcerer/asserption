450a92764674c4701a76df1812b8a3dab2c1c02e
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-06 01:18:02 -0300

    Cria falha específica para error de assert
    Com isso começamos a pensar em um vocabulário adequado para um framework
    de testes. A exceção `AssertionNotRaising` nos dá uma forma de falar
    sobre como testar algo que deveria lançar exceção mas não o fez.
    
    Agora podemos trabalhar para fazer "o teste passar", ou seja, lançar uma
    exceção do tipo `AssertionNotRaising` quando a expectation for falsa.

--- assert.rb
+++ assert.rb
@@ -1,5 +1,16 @@
+class AssertionNotRaising < StandardError; end
+
 def assert(expectation)
 
 end
 
+
+# a partir daqui, temos testes! :)
+
 assert true
+
+begin
+  assert false
+
+  raise AssertionNotRaising.new
+end
