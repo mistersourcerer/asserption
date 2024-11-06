5f07d18a903da2aa6638ce5d57a6dfbb4da7c644
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-06 12:16:03 -0300

    Cria teste vermelho para “não lançamento” de error
    Agora tenho um teste falhando que, quando passar, vai indicar que a
    função assert_raise falha se o bloco passado a ela não lança exceção.
    
    Próximo passo, fazer o teste passar.
    
    ps.: lembrando que, como não tenho um runner (ainda) uma falha de teste
    Significa simplesmente: rodar o programa em Ruby e ver uma exceção não
    capturada como resultado.

--- assert.rb
+++ assert.rb
@@ -1,2 +1,17 @@
+class AssertionNotRaising < StandardError; end
+
+def assert_raise(error_type, &expectation)
+  begin
+    expectation.call
+
+    # Se a execução chegou aqui é sinal que nenhuma exceção foi lançada pelo
+    # bloco, logo podemos considerar que o teste falhou.
+    raise AssertionNotRaising.new
+  rescue error_type
+    # Nada a fazer, significa que o teste passou porque a execução da
+    # expectation lançou a exceção esperada.
+  end
+end
+
 class NadaConstaError < StandardError; end
 assert_raise(NadaConstaError) {}
