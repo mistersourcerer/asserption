230ddc4de4bfa10e1a7f9cffc73a2e300c9a2df9
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-13 22:31:20 -0300

    Faz passar o teste de exceção sendo lançada.
    Porém, e se a exceção *não* for lançada? Deveríamos ver um erro,
    preferencialmente um erro específico que deixe claro que esse foi o
    problema.
    
    Será que temos implementação e também testes para garantir que isso
    está acontecendo? 🥶

--- assert.rb
+++ assert.rb
@@ -1,5 +1,10 @@
 def assert_raise(error_type, &expectation)
-  expectation.call
+  begin
+    expectation.call
+  rescue error_type
+    # Agora esperamos que o teste passe, já que o erro específico que desejamos
+    # foi lançado pelo bloco.
+  end
 end
 
 class NadaConstaError < StandardError; end
