5aea0c5d8165fb093a22274e26f3aeef884c9b92
Ricardo Valeriano <mister.sourcerer@gmail.com>
2024-11-13 22:38:48 -0300

    Cria erro específico para "exceção não lançada"
    Esse teste tenta garantir que, se uma exceção não for lançada por
    `assert_raise`, vemos um erro específico indicando isso.
    
    Porém o teste precisa passar agora. No momento temos um "teste falhando"
    que indica exatamente o que queremos, mas nosso intento e poder rodar
    esse teste e, se tudo estiver funcionando como queremos, é não ver uma
    exceção subindo...

--- assert.rb
+++ assert.rb
@@ -1,6 +1,12 @@
+class AssertionNotRaising < StandardError; end
+
 def assert_raise(error_type, &expectation)
   begin
     expectation.call
+
+    # se chegarmos aqui, significa que o bloco não lançou o erro do tipo
+    # específico `error_type`. Significa que o teste falhou.
+    raise AssertionNotRaising
   rescue error_type
     # Agora esperamos que o teste passe, já que o erro específico que desejamos
     # foi lançado pelo bloco.
@@ -9,3 +15,5 @@ end
 
 class NadaConstaError < StandardError; end
 assert_raise(NadaConstaError) { raise NadaConstaError }
+
+assert_raise(NadaConstaError) {}
